fd8d2aca90bd9ba1ec3be252803a59665bf38c9d
Sumit Kumar <sk498333@gmail.com>
2025-06-19 19:21:24 +0530

    Final Changes

---
+++ src/components/CountdownControl.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="controls">
+    <button @click="toggle">
+      {{ isRunning ? "Stop" : "Start" }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CountdownControl",
+  props: {
+    isRunning: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    toggle() {
+      this.$emit(this.isRunning ? "stop" : "start");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.controls button {
+  font-size: 1.2rem;
+  padding: 10px 20px;
+  margin: 10px 0;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/CountdownTimer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="timer">
+    <h2>{{ count }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CountdownTimer",
+  props: {
+    count: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.timer {
+  font-size: 3rem;
+  margin: 20px 0;
+}
+</style>
